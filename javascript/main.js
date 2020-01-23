b6de0ce6d1cf7989a3e3d50cdaaf21aadc403b11
lucapu88 <caputoluca88@gmail.com>
2020-01-23 16:24:49 +0100

    finito parte javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,22 @@
-// Eseguire una chiamata ajax all'api di boolean: https://flynn.boolean.careers/exercises/api/array/music che restituisce una lista di 10 dischi musicali.
-// Utilizzando handlebars, disegnare in pagina una card per ogni disco.
-// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
+
 $(document).ready(function(){
   var template_html = $('#myTemplate').html();//recupero il codice html del template
   var template_function = Handlebars.compile(template_html);//do in pasto a handlebars il codice html
   $.ajax({
-    url : 'https://flynn.boolean.careers/exercises/api/array/music',
+    url : 'data.php',
     method : 'get',
     success : function(data) {
-      var album = data.response //recupero l'array che è nell'url e contiene le proprietà di tutti gli album
-      for (var i = 0; i < album.length; i++) {
-        var context = { //creo la variabile con il contenuto che andrà nel template. I primi elementi sono quelli che ho assegnato nel div dell'html. Dopo i ':' ci sono i singoli valori del response che è dentro l'url (cioè il nostro API).
-          imgCopertina : album[i].poster,
-          titolo : album[i].title,
-          artista : album[i].author,
-          anno : album[i].year,
-          genere : album[i].genre
-        }
-        var disco = template_function(context); // utilizzando la funzione generata da handlebars, creo l'html finale
-        $('.dischi-container').append(disco); // infine vado ad appendere nel container il mio template che si ripeterà fino alla lunghezza dell'array response contenuto nell'API
+      var dischi = JSON.parse(data);
+      for (var i = 0; i < dischi.length; i++) {
+        var context = { //creo la variabile con il contenuto che andrà nel template.
+          imgCopertina : dischi[i].poster,
+          titolo : dischi[i].title,
+          artista : dischi[i].author,
+          anno : dischi[i].year,
+          genere : dischi[i].genre
+      }
+      var disco = template_function(context); // utilizzando la funzione generata da handlebars, creo l'html finale
+      $('.dischi-container').append(disco); // infine vado ad appendere nel container il mio template che si ripeterà fino alla lunghezza dell'array $dischi
       }
     },
     error : function(err) {
@@ -39,5 +37,5 @@ $(document).ready(function(){
         }
       });
     }
-  });
+  }); 
 });
